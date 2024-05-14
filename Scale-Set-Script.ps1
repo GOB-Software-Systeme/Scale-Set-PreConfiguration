b6a8dc9570235ee4138b1b6f188b5ae50ac055e9
Maurice Jennen <Maurice.Jennen@gob.de>
2024-05-14 16:01:28 +0200

    quick test

--- Scale-Set-Script.ps1
+++ Scale-Set-Script.ps1
@@ -1,10 +1,15 @@
-Write-Host "Set LongPathsEnabled to 1"
+Write-Host 'Set LongPathsEnabled to 1'
 
 $regKey = 'HKLM:\System\CurrentControlSet\Control\FileSystem'
 if ((Get-ItemProperty $regKey -Name 'LongPathsEnabled').LongPathsEnabled -eq 0) {
-    Set-ItemProperty $regKey -Name 'LongPathsEnabled' -value 1 -Force
+    Set-ItemProperty $regKey -Name 'LongPathsEnabled' -Value 1 -Force
 }
 
-Write-Host "Set NuGet PackageProvider"
+Write-Host 'Set NuGet PackageProvider'
 Install-PackageProvider NuGet -Force
 Import-PackageProvider NuGet -Force
+
+. '.\InstallOrUpdateDockerEngine.ps1' -Force -envScope 'Machine'
+
+Restart-Computer -Force
+Start-Service docker
